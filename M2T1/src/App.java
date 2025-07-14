330c17be99236cc6b53c002529dce46b68b91ced
NDanielR <n.danielramirez.h@outlook.com>
2025-07-13 20:41:49 -0500

    create class employee

--- M2T1/src/App.java
+++ M2T1/src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Employee employee1 = new Employee ("nelson ramirez", 12, 2000.0);
+        employee1.salaryIncrease(600);
+        Employee employee2 = new Employee ("jhorman ramirez",13,3000.0,0.0,"ventas");
+
+        System.out.println(employee1.toString());
+        System.out.println(employee2.toString());
+
     }
 }
---
+++ M2T1/src/Employee.java
@@ -0,0 +1,77 @@
+public class Employee {
+
+    private String name;
+
+    private Integer employeeCode;
+
+    private Double salary;
+
+    private Double bonus;
+
+    private String department;
+
+
+    //construtors
+    public Employee(String name, Integer employeeCode, Double salary, Double bonus, String department) {
+        this.name = name;
+        this.employeeCode = employeeCode;
+        this.salary = salary;
+        this.bonus = bonus;
+        this.department = department;
+    }
+
+    public Employee(String name, Integer employeeCode, Double salary, Double bonus) {
+        this(name, employeeCode, salary, bonus, "Indefinido");
+    }
+
+    public Employee(String name, Integer employeeCode, Double salary) {
+        this(name, employeeCode, salary,0.0,"Indefinido");
+    }
+
+    public Employee(String name, Integer employeeCode) {
+        this(name, employeeCode, 1250000.0, 0.0, "Indefinido");
+    }
+
+
+    //methods
+    public void salaryIncrease(double increase){
+        this.salary = salary + increase;
+    }
+
+    public void addBonus (double addBonus){
+        this.bonus = addBonus;
+    }
+
+    public void modifyDepartment(String modifDepartment){
+        this.department = modifDepartment;
+    }
+
+
+    //getters
+    private String getName() {
+        return name;
+    }
+
+    private Integer getEmployeeCode() {
+        return employeeCode;
+    }
+
+    private Double getSalary() {
+        return salary;
+    }
+
+    private Double getBonus() {
+        return bonus;
+    }
+
+    private String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return 
+        ("name = " + getName() + " employeeCode = " + getEmployeeCode() + " salary = " + getSalary() + " bonus = " + getBonus()
+        + " departamento = " + getDepartment());
+    }
+}
