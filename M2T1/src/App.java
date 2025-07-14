36ac2846f88e16926300f7fb1319d1e17d0df241
NDanielR <n.danielramirez.h@outlook.com>
2025-07-13 20:50:26 -0500

    add new employee

--- M2T1/src/App.java
+++ M2T1/src/App.java
@@ -3,9 +3,9 @@ public class App {
         Employee employee1 = new Employee ("nelson ramirez", 12, 2000.0);
         employee1.salaryIncrease(600);
         Employee employee2 = new Employee ("jhorman ramirez",13,3000.0,0.0,"ventas");
-
+        Employee employee3 = new Employee ();
         System.out.println(employee1.toString());
         System.out.println(employee2.toString());
-
+        System.out.println(employee3.toString());
     }
 }
